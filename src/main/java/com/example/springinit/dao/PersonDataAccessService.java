3c4fdc3e32fbe181efd74f3a6aecabe8f966d9fc
anuragdaksh7 <anuragdaksh77777@gmail.com>
2024-07-23 23:30:22 +0530

    changes

---
+++ src/main/java/com/example/springinit/dao/PersonDataAccessService.java
@@ -0,0 +1,52 @@
+package com.example.springinit.dao;
+
+import com.example.springinit.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        final String sql = "SELECT id, name FROM person";
+        jdbcTemplate.query(
+                sql, (resultSet, i) -> {
+                    return new Person(UUID.fromString(resultSet.getString("id")),resultSet.getString("name"));
+                }
+        );
+        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/example/springinit/service/PersonService.java
@@ -0,0 +1,43 @@
+package com.example.springinit.service;
+
+import com.example.springinit.dao.PersonDao;
+import com.example.springinit.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("postgres") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPersons() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person person) {
+        return personDao.updatePersonById(id, person);
+    }
+}
